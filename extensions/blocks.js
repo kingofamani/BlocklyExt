46e408088cf67a7788939b7bd666387fc1d40edd
林慶哲 <kingofamani@gmail.com>
2024-10-16 07:35:27 +0800

    新增：Serial ReadString

--- extensions/blocks.js
+++ extensions/blocks.js
@@ -270,6 +270,38 @@ Blockly.Blocks['_04amani_esp32joystick_sw'] = {
   }
 };
 
+Blockly.Blocks['_01amani_serial_avail'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("如果")
+        .appendField(new Blockly.FieldDropdown([["Serial","Serial"], ["Serial1","Serial1"], ["Serial2","Serial2"]]), "serial");
+    this.appendDummyInput()
+        .appendField("讀取到字串");
+    this.appendStatementInput("msg")
+        .setCheck(null);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(195);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['_02amani_serial_readstring'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["Serial","Serial"], ["Serial1","Serial1"], ["Serial2","Serial2"]]), "serial");
+    this.appendDummyInput()
+        .appendField("讀取到的完整字串內容");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(195);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
 /*
 =========================================
 =			i2cKeypadBlock				=
--- extensions/javascript.js
+++ extensions/javascript.js
@@ -170,6 +170,22 @@ Blockly.Arduino['_04amani_esp32joystick_sw'] = function(block) {
   return [code, Blockly.Arduino.ORDER_NONE];
 };
 
+Blockly.Arduino['_01amani_serial_avail'] = function(block) {
+  var dropdown_serial = block.getFieldValue('serial');
+  var statements_msg = Blockly.Arduino.statementToCode(block, 'msg');
+  // TODO: Assemble javascript into code variable.
+  var code ='if ('+dropdown_serial+'.available()) {\n ' + statements_msg + '}\n ';
+  return code;
+};
+
+Blockly.Arduino['_02amani_serial_readstring'] = function(block) {
+  var dropdown_serial = block.getFieldValue('serial');
+  // TODO: Assemble javascript into code variable.
+  var code = dropdown_serial+'.readString()';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Arduino.ORDER_NONE];
+};
+
 
 /*
 =========================================

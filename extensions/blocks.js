4a5d1f3c762333e03013a4ba774bcbf6f8171fcb
林慶哲 <kingofamani@gmail.com>
2024-10-30 09:26:37 +0800

    新增：UART序列埠

--- extensions/blocks.js
+++ extensions/blocks.js
@@ -302,6 +302,110 @@ Blockly.Blocks['_02amani_serial_readstring'] = {
   }
 };
 
+Blockly.Blocks['_01amani_uart_init'] = {
+  init: function() {
+	this.appendDummyInput()
+        .appendField("如果序列埠");
+    this.appendDummyInput()	 
+		.setAlign(Blockly.ALIGN_RIGHT)	
+		.appendField(new Blockly.FieldDropdown([
+			["Serial","Serial"],
+			["Serial1","Serial1"],
+			["Serial2","Serial2"],
+			["SoftwareSerial","softSerial"]				
+		], this.validate), "serial");
+    this.appendValueInput("rx","rx")
+        .setCheck("Number")
+		.appendField("RX");
+    this.appendValueInput("tx","tx")
+        .setCheck("Number")
+		.appendField("TX");
+	this.getInput("rx").setVisible(false);
+	this.getInput("tx").setVisible(false);		
+    this.appendDummyInput()	 
+		.setAlign(Blockly.ALIGN_RIGHT)		
+		.appendField("鮑率")	
+		.appendField(new Blockly.FieldDropdown([
+			["300","300"],
+			["1200","1200"],
+			["2400","2400"],
+			["4800","4800"],
+			["9600","9600"],	    
+			["19200","19200"],
+			["38400","38400"],
+			["57600","57600"],
+			["74880","74880"],
+			["115200","115200"],	
+			["230400","230400"],
+			["250000","250000"],
+			["500000","500000"],
+			["1000000","1000000"],	
+			["2000000","2000000"]
+		]), "baudrate");
+	this.appendDummyInput()
+        .appendField("取得字串");			
+    this.appendStatementInput("statement")
+        .setCheck(null);
+	this.setInputsInline(true);
+	this.setPreviousStatement(!0);
+	this.setNextStatement(!0);
+	this.setColour(225);
+  },
+  validate: function(newValue) {
+	 const block = this.sourceBlock_;
+	 if (newValue=="Serial") {
+		 block.getInput("rx").setVisible(false);
+		 block.getInput("tx").setVisible(false);		 
+	 }
+	 else {
+		 block.getInput("rx").setVisible(true);
+		 block.getInput("tx").setVisible(true);		 
+	 } 
+  }
+};
+
+Blockly.Blocks['_02amani_uart_get'] = {
+  init: function() {
+	this.appendDummyInput()
+        .appendField("序列埠");	  
+	this.appendDummyInput()
+        .appendField("讀取完整的字串");   
+	this.setInputsInline(true);
+    this.setOutput(true, null);  
+    this.setColour(225);
+  }
+};
+
+Blockly.Blocks['_03amani_uart_send'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("序列埠");
+    this.appendDummyInput()	 
+		.setAlign(Blockly.ALIGN_RIGHT)	
+		.appendField(new Blockly.FieldDropdown([
+			["Serial","Serial"],
+			["Serial1","Serial1"],
+			["Serial2","Serial2"],
+			["SoftwareSerial","softSerial"]
+		]), "serial");
+	this.appendDummyInput()	 
+		.setAlign(Blockly.ALIGN_RIGHT)	
+		.appendField(new Blockly.FieldDropdown([
+			["傳送資料(print)","print"],
+			["傳送資料(print換行)","println"],
+			["傳送資料(write)","write"]
+		]), "send");
+	this.appendValueInput("msg")
+        .setCheck(null)
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(225);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
 /*
 =========================================
 =			i2cKeypadBlock				=
--- extensions/javascript.js
+++ extensions/javascript.js
@@ -187,6 +187,65 @@ Blockly.Arduino['_02amani_serial_readstring'] = function(block) {
 };
 
 
+Blockly.Arduino['_01amani_uart_init'] = function(block) {
+	var serial = block.getFieldValue('serial');
+	var rx = Blockly.Arduino.valueToCode(block, 'rx', Blockly.Arduino.ORDER_ATOMIC);  	
+	var tx = Blockly.Arduino.valueToCode(block, 'tx', Blockly.Arduino.ORDER_ATOMIC); 	
+    var baudrate = block.getFieldValue('baudrate');	
+	var statement = Blockly.Arduino.statementToCode(block, 'statement');
+	
+	if (serial=="Serial") {
+		if ('setupsTop_' in Blockly.Arduino)
+			Blockly.Arduino.setupsTop_.setup_serial = serial+'.begin('+baudrate+');\n  delay(10);\n  ';
+		else
+			Blockly.Arduino.setups_.setup_serial = serial+'.begin('+baudrate+');\n  delay(10);\n  ';
+	}
+	else if (serial=="Serial1" || serial=="Serial2") {
+		if ('setupsTop_' in Blockly.Arduino)
+			Blockly.Arduino.setupsTop_["define_HardwareSerial_"+serial] = serial+'.begin('+baudrate+', SERIAL_8N1, '+rx+', '+tx+');\n  delay(10);\n  ';
+		else
+			Blockly.Arduino.setups_["define_HardwareSerial_"+serial] = serial+'.begin('+baudrate+', SERIAL_8N1, '+rx+', '+tx+');\n  delay(10);\n';	
+	}	
+	else if  (serial=="softSerial") {
+		Blockly.Arduino.definitions_["define_SoftwareSerial"] = '#include <SoftwareSerial.h>\nSoftwareSerial '+serial+'('+rx+', '+tx+');';
+		if ('setupsTop_' in Blockly.Arduino)
+			Blockly.Arduino.setupsTop_["define_SoftwareSerial_"+serial] = serial+'.begin('+baudrate+');\n  delay(10);\n  ';
+		else
+			Blockly.Arduino.setups_["define_SoftwareSerial_"+serial] = serial+'.begin('+baudrate+');\n  delay(10);\n';
+	}
+	
+	var code;
+	code =	'if ('+serial+'.available()) {\n'+
+				'  String serialMsg = "";\n'+
+				'  while ('+serial+'.available()) {\n'+
+				'    serialMsg = '+serial+'.readString();\n  '+statement+
+				'  }\n'+	
+				'}\n';
+	
+  return code;
+};
+
+
+Blockly.Arduino['_02amani_uart_get'] = function(block) {
+  code = 'serialMsg';
+  return [code, Blockly.Arduino.ORDER_NONE];
+};
+
+Blockly.Arduino['_03amani_uart_send'] = function(block) {
+  var serial = block.getFieldValue('serial'); 
+  var send = block.getFieldValue('send'); 
+  var value_msg = Blockly.Arduino.valueToCode(block, 'msg', Blockly.Arduino.ORDER_ATOMIC);
+  var code = "";
+  if(send == "print"){
+	code = serial+'.print(%1);\n'.replace("%1", value_msg);
+  }else if(send == "println"){
+	code = serial+'.println(%1);\n'.replace("%1", value_msg);
+  }else if(send == "write"){
+	code = serial+'.write(%1);\n'.replace("%1", value_msg);
+  }
+  return code;
+};
+
 /*
 =========================================
 =			i2cKeypadBlock				=

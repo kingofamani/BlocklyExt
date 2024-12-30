1a69ee8cce8072c825e12327e11b8619724bd6e5
林慶哲 <kingofamani@gmail.com>
2024-12-31 07:17:03 +0800

    新增：AI教室

--- extensions/blocks.js
+++ extensions/blocks.js
@@ -734,3 +734,116 @@ Blockly.Blocks['amani_mqtt_received_string'] = {
   }
 };
 
+
+/*
+=========================================
+=			AI Classroom(test)			=
+=========================================
+*/
+Blockly.Blocks['amani_fake_ina226_init'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("初始化INA226數位電表 編號")
+        .appendField(new Blockly.FieldDropdown([["0","0"], ["1","1"], ["2","2"]]), "inachannel");
+    this.setOutput(true, null);
+    this.setColour(75);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['amani_fake_ina226_mv'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("分流電壓mV 編號")
+        .appendField(new Blockly.FieldDropdown([["0","0"], ["1","1"], ["2","2"]]), "mvchannel");
+    this.setOutput(true, null);
+    this.setColour(75);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['amani_fake_ina226_ma'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("電流mV 編號")
+        .appendField(new Blockly.FieldDropdown([["0","0"], ["1","1"], ["2","2"]]), "machannel");
+    this.setOutput(true, null);
+    this.setColour(75);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['amani_fake_cnc_init'] = {
+  init: function() {
+    this.appendValueInput("cncenable")
+        .setCheck(null)
+        .appendField("步進馬達 CNC控制板 致能");
+    this.appendValueInput("xdir")
+        .setCheck(null)
+        .appendField("X方向");
+    this.appendValueInput("xstep")
+        .setCheck(null)
+        .appendField("X步進");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(195);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['amani_fake_cnc_xmove'] = {
+  init: function() {
+    this.appendValueInput("xmove")
+        .setCheck(null)
+        .appendField("馬達X軸移動至");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(195);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['amani_fake_cnc_xcurrent'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("取得目前步進馬達X軸位置");
+    this.setOutput(true, null);
+    this.setColour(195);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['amani_fake_yfs201_init'] = {
+  init: function() {
+    this.appendValueInput("yfspin")
+        .setCheck(null)
+        .appendField("YF-S201水流量感測器 初始化 腳位");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(0);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['amani_fake_yfs201_flow'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("累計出水量(mL/s)");
+    this.setOutput(true, null);
+    this.setColour(0);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+
--- extensions/javascript.js
+++ extensions/javascript.js
@@ -591,8 +591,71 @@ Blockly.Arduino['amani_mqtt_received_string'] = function(block) {
   return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
 
+/*
+=========================================
+=			AI Classroom(test)			=
+=========================================
+*/
+Blockly.Arduino['amani_fake_ina226_init'] = function(block) {
+  var dropdown_inachannel = block.getFieldValue('inachannel');
+  // TODO: Assemble javascript into code variable.
+  var code = '';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Arduino.ORDER_NONE];
+};
 
+Blockly.Arduino['amani_fake_ina226_mv'] = function(block) {
+  var dropdown_inachannel = block.getFieldValue('mvchannel');
+  // TODO: Assemble javascript into code variable.
+  var code = '';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Arduino.ORDER_NONE];
+};
 
+Blockly.Arduino['amani_fake_ina226_ma'] = function(block) {
+  var dropdown_inachannel = block.getFieldValue('machannel');
+  // TODO: Assemble javascript into code variable.
+  var code = '';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Arduino.ORDER_NONE];
+};
+
+Blockly.Arduino['amani_fake_cnc_init'] = function(block) {
+  var value_cncenable = Blockly.Arduino.valueToCode(block, 'cncenable', Blockly.Arduino.ORDER_ATOMIC);
+  var value_xdir = Blockly.Arduino.valueToCode(block, 'xdir', Blockly.Arduino.ORDER_ATOMIC);
+  var value_xstep = Blockly.Arduino.valueToCode(block, 'xstep', Blockly.Arduino.ORDER_ATOMIC);
+  // TODO: Assemble javascript into code variable.
+  var code = '';
+  return code;
+};
+
+Blockly.Arduino['amani_fake_cnc_xmove'] = function(block) {
+  var value_xmove = Blockly.Arduino.valueToCode(block, 'xmove', Blockly.Arduino.ORDER_ATOMIC);
+  // TODO: Assemble javascript into code variable.
+  var code = '';
+  return code;
+};
+
+Blockly.Arduino['amani_fake_cnc_xcurrent'] = function(block) {
+  // TODO: Assemble javascript into code variable.
+  var code = '';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Arduino.ORDER_NONE];
+};
+
+Blockly.Arduino['amani_fake_yfs201_init'] = function(block) {
+  var value_yfspin = Blockly.Arduino.valueToCode(block, 'yfspin', Blockly.Arduino.ORDER_ATOMIC);
+  // TODO: Assemble javascript into code variable.
+  var code = '';
+  return code;
+};
+
+Blockly.Arduino['amani_fake_yfs201_flow'] = function(block) {
+  // TODO: Assemble javascript into code variable.
+  var code = '';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Arduino.ORDER_NONE];
+};
 
 
 
